85160a1708a47d6dc55e65ca8e924158d335465d
Matthew Sciamanna <mattsciamanna@Matthews-MacBook-Air-5.local>
2018-12-17 16:09:13 -0500

    messing with the training

--- qTraining.py
+++ qTraining.py
@@ -29,4 +29,23 @@ def train1():
             print player.weights
             print 'Training 1: pickled weights:', pickle.dumps(player.weights)
 
+featureSet2 = [SizeofHand(), OpponentCards(), Illegality()]
+def train2():
+    qBot1 = QLearner('qBot1', featureSet2) #default values
+    pBot2 = HmmAgent("A2")
+    
+    random.seed(182)
+    g = Game([qBot1, pBot2], True)
+    g.playGame(5000)
+
+    # #print stats
+    for player in g.players:
+        print player.name
+        print player.wins
+        if type(player) == LearningAgent or type(player) == RandomAgent or type(player) == HmmAgent or type(player) == HeuristicAgent:
+            print np.average(player.validPercentByRound)
+        if type(player) == QLearner:
+            print player.weights
+            print 'Training 1: pickled weights:', pickle.dumps(player.weights)
+
 # see output.py for the pickled output
